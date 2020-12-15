fa1dc5fcbf60fbb7816311964ccdcdd4464abae8
Pablo Marcos Sánchez <pablomarcossanche06.pm@gmail.com>
2020-12-15 20:36:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pokemon/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.pokemon
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val boton = findViewById<Button>(R.id.button)
+
+        boton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+}
